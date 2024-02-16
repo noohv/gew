f7a87279f51d0dd946804e160d2e405cf0c86cf7
noohv <valters.huuns@gmail.com>
2024-02-16 22:35:19 +0200

    Test

---
+++ src/emotions.js
@@ -0,0 +1,84 @@
+const emotions = [
+    {
+        id: "em01",
+        name: "Interese"
+    },
+    {
+        id: "em02",
+        name: "Uzjautrinājums"
+    },
+    {
+        id: "em03",
+        name: "Lepnums"
+    },
+    {
+        id: "em04",
+        name: "Prieks"
+    },
+    {
+        id: "em05",
+        name: "Bauda"
+    },
+    {
+        id: "em06",
+        name: "Apmierinātība"
+    },
+    {
+        id: "em07",
+        name: "Mīlestība"
+    },
+    {
+        id: "em08",
+        name: "Apbrīna"
+    },
+    {
+        id: "em09",
+        name: "Atvieglojums"
+    },
+    {
+        id: "em10",
+        name: "Līdzjūtība"
+    },
+    {
+        id: "em11",
+        name: "Skumjas"
+    },
+    {
+        id: "em12",
+        name: "Vaina"
+    },
+    {
+        id: "em13",
+        name: "Nožēla"
+    },
+    {
+        id: "em14",
+        name: "Kauns"
+    },
+    {
+        id: "em15",
+        name: "Vilšanās"
+    },
+    {
+        id: "em16",
+        name: "Bailes"
+    },
+    {
+        id: "em17",
+        name: "Riebums"
+    },
+    {
+        id: "em18",
+        name: "Nicinājums"
+    },
+    {
+        id: "em19",
+        name: "Naids"
+    },
+    {
+        id: "em20",
+        name: "Dusmas"
+    }
+]
+
+export default emotions
